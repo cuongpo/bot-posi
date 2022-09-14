32e97e7f3d07602b827e147c1f9e9998a7cc92d0
cuongpham <61429471+cuongpo@users.noreply.github.com>
2022-09-14 09:47:55 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
+// Get price from binance
 const response = await fetch('https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT');
 const myJson = await response.json(); //extract JSON from the http response
 const price = myJson.price;
-console.log(price-1);
+console.log(price);
+
+// Get price from Posi
+
+// Check diffrerent between those price
+
+// Call API telegram when different > xx 
+
+
+
+
